cd7bd9d3dda2bffba8f03aaa0cc9c831d8673829
Nicolas Lacroix <nicolas.lacroix@univ-cotedazur.fr>
2025-01-09 11:15:49 +0100

    [colombus] Updating client to match api/db changes (Workflow to Profile renaming)

--- ui/src/api/client.ts
+++ ui/src/api/client.ts
@@ -28,15 +28,15 @@ export interface GetNodesFromNeo4JResponse {
 	};
 }
 
-export async function getNodesFromNeo4J(workflowNames?: string[]) {
-	if (workflowNames?.length === 0) {
+export async function getNodesFromNeo4J(profilesNames?: string[]) {
+	if (profilesNames?.length === 0) {
 		return new Promise<Neo4JGraphDefinition>(() => [] as Neo4JGraphDefinition);
 	}
 	return await axios
 		.post<GetNodesFromNeo4JResponse>(
 			"http://localhost:7474/db/neo4j/query/v2",
 			{
-				statement: `MATCH (w:Workflow)-[wsa]->(sa:Stage)-[sase]->(se:Step)-[sem]->(m:MetaInstruction)-[mc]->(c:Code) OPTIONAL MATCH (sa)-[sasp:PRECEDES]->(:Stage) OPTIONAL MATCH (se)-[sesp:PRECEDES]->(:Step) OPTIONAL MATCH (m)-[mp:PRECEDES]->(:MetaInstruction) OPTIONAL MATCH (c)-[cp:PRECEDES]->(:Code) MATCH (sa)-[cosase:CONTAINS]->(:Step) WITH w, sa, se, m, c, wsa, sase, sem, mc, sasp, sesp, mp, cp, count(cosase) AS count_sa_se_children MATCH (se)-[cosem:REFERS_TO]->(:MetaInstruction) WITH w, sa, se, m, c, wsa, sase, sem, mc, sasp, sesp, mp, cp, count_sa_se_children, count(cosem) AS count_se_m_children WHERE ANY (name in w.name WHERE name IN ${JSON.stringify(workflowNames)}) RETURN w, sa, se, m, c, wsa, sase, sem, mc, sasp, sesp, mp, cp, count_sa_se_children, count_se_m_children`,
+				statement: `MATCH (p:Profile)-[psa]->(sa:Stage)-[sase]->(se:Step)-[sem]->(m:MetaInstruction)-[mc]->(c:Code) OPTIONAL MATCH (sa)-[sasp:PRECEDES]->(:Stage) OPTIONAL MATCH (se)-[sesp:PRECEDES]->(:Step) OPTIONAL MATCH (m)-[mp:PRECEDES]->(:MetaInstruction) OPTIONAL MATCH (c)-[cp:PRECEDES]->(:Code) MATCH (sa)-[cosase:CONTAINS]->(:Step) WITH p, sa, se, m, c, psa, sase, sem, mc, sasp, sesp, mp, cp, count(cosase) AS count_sa_se_children MATCH (se)-[cosem:REFERS_TO]->(:MetaInstruction) WITH p, sa, se, m, c, psa, sase, sem, mc, sasp, sesp, mp, cp, count_sa_se_children, count(cosem) AS count_se_m_children WHERE ANY (name in p.name WHERE name IN ${JSON.stringify(profilesNames)}) RETURN p, sa, se, m, c, psa, sase, sem, mc, sasp, sesp, mp, cp, count_sa_se_children, count_se_m_children`,
 			},
 			config,
 		)
