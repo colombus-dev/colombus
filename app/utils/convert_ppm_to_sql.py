28503623a09b15892a73e3a36a3d9bcdf573ff52
Nicolas Lacroix <nicolas.lacroix@univ-cotedazur.fr>
2025-03-18 10:40:10 +0100

    Tmp reverting ce10c8e (all metainstructions in ppm by default)

--- app/utils/convert_ppm_to_sql.py
+++ app/utils/convert_ppm_to_sql.py
@@ -51,11 +51,12 @@ def convert_steps_to_sql_query_template(
         {
             "is_required": g.multiplicity != "*",
             "meta_instructions": __reduce_meta_instructions(g.metaInstructions or [])
-            or [
-                PatternMetaInstruction(
-                    algoFamily=None, algoName=None, library=None, function=None
-                )
-            ],
+            # TODO
+            # or [
+            #     PatternMetaInstruction(
+            #         algoFamily=None, algoName=None, library=None, function=None
+            #     )
+            # ],
         }
         for g in flat_pattern
         if g.steps
