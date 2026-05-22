5daad6eb3d28ec71cc5f52c3b10bd01c9f7d769e
Yassin Mohammed Umer <yassinmohammedumer@gmail.com>
2026-05-22 11:09:01 +0200

    fix(app/settings.py): temporarily allow all origins in dev mode

--- app/settings.py
+++ app/settings.py
@@ -35,8 +35,10 @@ class Settings(BaseSettings):
 
     @model_validator(mode="after")
     def set_allowed_origins(self) -> "Settings":
-        if not self.allowed_origins:
+        if self.is_production:
             self.allowed_origins = [f"{self.ui_host}", f"{self.ui_host}:{self.ui_port}"]
+        else:
+            self.allowed_origins = ["*"]
         return self
 
 
