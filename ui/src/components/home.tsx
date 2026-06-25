d372b427d4684a964646fe02c087142850e010ac
alai06 <allaheddine.cherigui@gmail.com>
2026-06-25 13:14:34 +0200

    fix(ui): fix the display of the card when you log in to the app for the first time

--- ui/src/components/home.tsx
+++ ui/src/components/home.tsx
@@ -24,6 +24,7 @@ export default function Home() {
 
 			fetch(`${baseURL}/project`, {
 				headers: { "x-api-key": jwtToken },
+				cache: "no-store",
 			})
 				.then((res) => {
 					if (!res.ok) throw new Error("Failed to fetch projects");
